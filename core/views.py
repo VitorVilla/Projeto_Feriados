fdcc8c1bc500c6444f56f3a6acc9aaad5660610d
Vitor Villa <80797997+VitorVilla@users.noreply.github.com>
2023-04-02 19:31:13 -0300

    ajustando erros

--- core/views.py
+++ core/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from datetime import datetime
 
-from django.http import HttpResponse
-
 contexto = {
-    "natal":False,
-    "tiradentes":False
+    "natal": False,
+    "tiradentes": False
 }
+
+
 def natal(request):
-    if datetime.now().month == 12 and datetime.now().day == 25:
+    if datetime.now().day == 25 and datetime.now().month == 12:
         contexto["Hoje é natal"] = True
     return render(request, 'natal.html', contexto)
 
+
 def tiradentes(request):
-    if datetime.now().month == 4 and datetime.now().day == 20:
+    if datetime.now().day == 20 and datetime.now().month == 4:
         contexto["Hoje é tiradentes"] = True
     return render(request, 'tiradentes.html', contexto)
